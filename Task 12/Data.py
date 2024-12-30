6766d242e5bb4b714427e647b3a19f58c443f582
naveenkumar-1501 <naveenkumar15012001@gmail.com>
2024-12-30 15:13:45 +0530

    Add files via upload

---
+++ Task 12/Data.py
@@ -0,0 +1,16 @@
+"""
+Data.py
+"""
+
+class WebData:
+    # URL for the login page of the application
+    URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+
+    # URL for the dashboard page after successful login
+    DASHBOARD_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+
+    # Path to the Excel file containing test data
+    EXCEL_FILE = "E:\\Guvi\\Python\\Task\\Task 12\\test_data.xlsx"
+
+    # Sheet name or number in the Excel file to be used for test data
+    SHEET_NUMBER = "Sheet1"
---
+++ Task 12/Locators.py
@@ -0,0 +1,13 @@
+"""
+Locators.py
+"""
+
+class TestLocators:
+    # Locator for the username field
+    usernameLocator = "username"
+    # Locator for the password field
+    passwordLocator = "password"
+    # Locator for the submit button (using XPath)
+    submitButton = "//button[@type='submit']"
+    # Locator for the logout button (using XPath)
+    logoutButton = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/ul/li[4]/a'
