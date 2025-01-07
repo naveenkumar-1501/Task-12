04131a0c129bff5f47045d96bb8e89198feaf6e2
naveenkumar-1501 <naveenkumar15012001@gmail.com>
2025-01-07 20:05:05 +0530

    Locators.py

--- Task 12/Locators.py
+++ Task 12/Locators.py
@@ -9,5 +9,7 @@ class TestLocators:
     passwordLocator = "password"
     # Locator for the submit button (using XPath)
     submitButton = "//button[@type='submit']"
+    # Locator for the dropdown (using Xpath)
+    dropdown = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/span/img'
     # Locator for the logout button (using XPath)
     logoutButton = '//*[@id="app"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/ul/li[4]/a'
